e244ab1b9ec80c7c81cbc6a43f1e209a8f349a8c
cnpiensadigital <cnpiensadigital@gmail.com>
2018-08-10 22:35:10 -0500

    Commit inicial

---
+++ app/views/campaigns/_campaign.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! campaign, :id, :name, :account_id, :created_at, :updated_at
+json.url campaign_url(campaign, format: :json)
---
+++ db/migrate/20180804054206_create_campaigns.rb
@@ -0,0 +1,10 @@
+class CreateCampaigns < ActiveRecord::Migration[5.1]
+  def change
+    create_table :campaigns do |t|
+      t.string :name
+      t.belongs_to :account, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
