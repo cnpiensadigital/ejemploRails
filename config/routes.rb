9211ca37bb690782135890be5890103d71f1ebde
cnpiensadigital <cnpiensadigital@gmail.com>
2018-08-11 01:45:53 -0500

    Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   resources :comments
+  scope 'admin' do
+	resources :posts
+  end
+  
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
